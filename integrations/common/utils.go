4035c302abfa931cba467bf9f0f4800b6dffa052
Alexander Gil <pando855@gmail.com>
2024-02-25 21:00:46 +0100

    refactor(integrations): Add common code to a library

---
+++ integrations/common/utils.go
@@ -0,0 +1,123 @@
+package common
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type ScheduledItem struct {
+	SourcePath      string      `json:"source_path"`
+	DestinationPath string      `json:"destination_path"`
+	ID              string      `json:"id"`
+	Events          interface{} `json:"events"`
+}
+
+type FailedItem struct {
+	SourcePath      string `json:"source_path"`
+	DestinationPath string `json:"destination_path"`
+	ForceCompleted  bool   `json:"forceCompleted"`
+	ForceFailed     bool   `json:"forceFailed"`
+	ForceExecuting  bool   `json:"forceExecuting"`
+	ForceQueued     bool   `json:"forceQueued"`
+	Error           string `json:"error"`
+}
+
+type Response struct {
+	Scheduled []ScheduledItem `json:"scheduled"`
+	Failed    []FailedItem    `json:"failed"`
+	Skipped   interface{}     `json:"skipped"`
+}
+
+func PrintGearrResponse(jsonStr []byte) error {
+	var response Response
+	if err := json.Unmarshal(jsonStr, &response); err != nil {
+		return err
+	}
+
+	switch {
+	case len(response.Scheduled) > 0:
+		fmt.Println("Item successfully queued.")
+	case len(response.Failed) > 0:
+		fmt.Println("Item was not queued.")
+	default:
+		return fmt.Errorf("Item was neither queued nor failed")
+	}
+
+	return nil
+}
+
+func AddToGearrQueue(path string, url string, token string, itemType string) error {
+	payload := map[string]string{
+		"source_path": path,
+	}
+
+	payloadJSON, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadJSON))
+	if err != nil {
+		return err
+	}
+
+	authHeader := fmt.Sprintf("Bearer %s", token)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", authHeader)
+
+	client := http.Client{}
+
+	fmt.Printf("Adding %s to gearr queue\n", itemType)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = PrintGearrResponse(body)
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "Failed with status %s and message: %s", resp.Status, body)
+	}
+
+	fmt.Println()
+	fmt.Println()
+	return nil
+}
+
+func IsNotCodec(codec string, allowedCodecs []string) bool {
+	lowerCodec := strings.ToLower(codec)
+	for _, allowed := range allowedCodecs {
+		if lowerCodec == allowed {
+			return false
+		}
+	}
+	return true
+}
+
+func HumanReadableSize(size int64) string {
+	if size == 0 {
+		return "N/A"
+	}
+
+	const unit = 1024
+	if size < unit {
+		return fmt.Sprintf("%d B", size)
+	}
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.2f %cB", float64(size)/float64(div), "KMGTPE"[exp])
+}
--- integrations/radarr/add/main.go
+++ integrations/radarr/add/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"sort"
 	"strings"
+
+	"gearr/integrations/common"
 
 	"github.com/spf13/pflag"
 	"golift.io/starr"
@@ -32,109 +29,7 @@ func getSize(m *radarr.Movie) int64 {
 func isNotX265OrH265(m *radarr.Movie) bool {
 	videoCodec := strings.ToLower(m.MovieFile.MediaInfo.VideoCodec)
 	return m.MovieFile != nil && m.MovieFile.MediaInfo != nil &&
-		(videoCodec != "x265" && videoCodec != "h265" && videoCodec != "hevc")
-}
-
-func HumanReadableSize(size int64) string {
-	if size == 0 {
-		return "N/A"
-	}
-
-	const unit = 1024
-	if size < unit {
-		return fmt.Sprintf("%d B", size)
-	}
-	div, exp := int64(unit), 0
-	for n := size / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.2f %cB", float64(size)/float64(div), "KMGTPE"[exp])
-}
-
-type ScheduledItem struct {
-	SourcePath      string      `json:"source_path"`
-	DestinationPath string      `json:"destination_path"`
-	ID              string      `json:"id"`
-	Events          interface{} `json:"events"`
-}
-
-type FailedItem struct {
-	SourcePath      string `json:"source_path"`
-	DestinationPath string `json:"destination_path"`
-	ForceCompleted  bool   `json:"forceCompleted"`
-	ForceFailed     bool   `json:"forceFailed"`
-	ForceExecuting  bool   `json:"forceExecuting"`
-	ForceQueued     bool   `json:"forceQueued"`
-	Error           string `json:"error"`
-}
-
-type Response struct {
-	Scheduled []ScheduledItem `json:"scheduled"`
-	Failed    []FailedItem    `json:"failed"`
-	Skipped   interface{}     `json:"skipped"`
-}
-
-func PrintGearrResponse(jsonStr []byte) error {
-	var response Response
-	if err := json.Unmarshal(jsonStr, &response); err != nil {
-		return err
-	}
-
-	switch {
-	case len(response.Scheduled) > 0:
-		fmt.Println("Movie successfully queued.")
-	case len(response.Failed) > 0:
-		fmt.Println("Movie was not queued.")
-	default:
-		return errors.New("Movie was neither queued nor failed.")
-	}
-
-	return nil
-}
-
-func AddMovieToGearrQueue(path string, url string, token string) error {
-	payload := map[string]string{
-		"source_path": path,
-	}
-
-	payloadJSON, err := json.Marshal(payload)
-	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadJSON))
-	if err != nil {
-		return err
-	}
-
-	authHeader := fmt.Sprintf("Bearer %s", token)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", authHeader)
-
-	client := http.Client{}
-
-	fmt.Println("Adding movie to gearr queue")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	err = PrintGearrResponse(body)
-	if resp.StatusCode != http.StatusOK {
-		fmt.Fprintf(os.Stderr, "Failed with status %s and message: %s", resp.Status, body)
-	}
-
-	fmt.Println()
-	fmt.Println()
-	return nil
+		common.IsNotCodec(videoCodec, []string{"x265", "h265", "hevc"})
 }
 
 func main() {
@@ -185,11 +80,11 @@ func main() {
 		if m.MovieFile != nil {
 			fmt.Printf("Codec: %s\n", m.MovieFile.MediaInfo.VideoCodec)
 
-			fmt.Printf("Size: %s\n", HumanReadableSize(getSize(m)))
+			fmt.Printf("Size: %s\n", common.HumanReadableSize(getSize(m)))
 			fmt.Printf("Full Path: %s\n\n", m.MovieFile.Path)
 
 			if !*dryRun {
-				err := AddMovieToGearrQueue(m.MovieFile.Path, gearrPostURL, *gearrToken)
+				err := common.AddToGearrQueue(m.MovieFile.Path, gearrPostURL, *gearrToken, "movie")
 				if err != nil {
 					fmt.Println("error:", err)
 					os.Exit(1)
